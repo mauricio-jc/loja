a0b1261e52c05b5da9e0ec98fceefab19f8cc410
Maurício Citadini Biléssimo <mauricioc.bilessimo@gmail.com>
2023-07-04 17:39:03 -0300

    Colocado Bearer Token nos Headers das requisições.

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Categorie } from '../interfaces/categorie';
+import { UsersService } from './users.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,34 +11,37 @@ import { Categorie } from '../interfaces/categorie';
 export class CategoriesService {
 
   private api: string = environment.api;
+  private headers: Object = {};
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(
+    private httpClient: HttpClient,
+    private usersService: UsersService
+  ) {
+    this.headers = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${this.usersService.getAccessToken()}`
+      })
+    }
+  }
 
   listAll(): Observable<Categorie[]> {
-    return this.httpClient.get<Categorie[]>(`${this.api}/categories`);
+    return this.httpClient.get<Categorie[]>(`${this.api}/categories`, this.headers);
   }
 
   find(id: string): Observable<Categorie> {
-    return this.httpClient.get<Categorie>(`${this.api}/categories/${id}`);
+    return this.httpClient.get<Categorie>(`${this.api}/categories/${id}`, this.headers);
   }
 
   create(categorie: Categorie): Observable<Categorie> {
-    return this.httpClient.post<Categorie>(`${this.api}/categories/create`, categorie);
+    return this.httpClient.post<Categorie>(`${this.api}/categories/create`, categorie, this.headers);
   }
 
   update(id: string, name: {}): Observable<any> {
-    return this.httpClient.put<any>(`${this.api}/categories/edit/${id}`, name);
+    return this.httpClient.put<any>(`${this.api}/categories/edit/${id}`, name, this.headers);
   }
 
   delete(id: number): Observable<any> {
-    return this.httpClient.delete<any>(`${this.api}/categories/delete/${id}`);
+    return this.httpClient.delete<any>(`${this.api}/categories/delete/${id}`, this.headers);
   }
-
-  // listAll(): Observable<Categories[]> {
-  //   return this._httpClient.get<Office[]>(`${this._endpoint}/office`).pipe(
-  //     tap((offices) => {
-  //       this._offices.next(offices);
-  //     })
-  //   );
-  // }
 }
